922c4c8b74c9708224a63048d9aaeb411925b874
dana972 <72210081@students.liu.edu.lb>
2024-11-19 19:02:16 +0200

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid header-top sticky-top">
+            <div class="container d-flex align-items-center">
+                <div class="d-flex align-items-center h-100">
+                    <a href="#" class="navbar-brand" style="height: 125px;margin-left:0px">
+                        <h1 class=" mb-0"><i class="fas fa-leaf"></i> Flora</h1>
+                        <!-- <img src="img/logo.png" alt="Logo"> -->
+                    </a>
+                </div>
+                <div class="w-100 h-100">
+                    <div class="topbar px-0 py-2 d-none d-lg-block" style="height: 45px;">
+                        <div class="row gx-0 align-items-center">
+                            <div class="col-lg-8 text-center text-lg-center mb-lg-0">
+                                <div class="d-flex flex-wrap">
+                                    <div class="border-end  pe-3">
+                                        <a href="#" class="text-muted small"><i class="fas fa-map-marker-alt  me-2"></i>Find A Location</a>
+                                    </div>
+                                    <div class="ps-3">
+                                        <a href="mailto:example@gmail.com" class="text-muted small"><i class="fas fa-envelope  me-2"></i>example@gmail.com</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 text-center text-lg-end">
+                                <div class="d-flex justify-content-end">
+                                    <div class="d-flex border-end  pe-3">
+                                        <a class="btn p-0  me-3" href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a class="btn p-0  me-3" href="#"><i class="fab fa-twitter"></i></a>
+                                        <a class="btn p-0  me-3" href="#"><i class="fab fa-instagram"></i></a>
+                                        <a class="btn p-0  me-0" href="#"><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                    <div class="dropdown ms-3">
+                                        <a href="#" class="dropdown-toggle " data-bs-toggle="dropdown"><small class="text-body"><i class="fas fa-globe-europe  me-2"></i> English</small></a>
+                                        <div class="dropdown-menu">
+                                            <a href="#" class="dropdown-item">English</a>
+                                            <a href="#" class="dropdown-item">Spanish</a>
+                                            <a href="#" class="dropdown-item">Arabic</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nav-bar px-0 py-lg-0" style="height: 80px;">
+                        <nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-lg-end">
+                            <a href="#" class="navbar-brand-2">
+                                <h1 class=" mb-0 "><i class="fas fa-leaf"></i> Flora</h1>
+                                <!-- <img src="img/logo.png" alt="Logo"> -->
+                            </a>  
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                                <span class="fa fa-bars"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarCollapse">
+                                <div class="navbar-nav mx-0 mx-lg-auto bg-white">
+                                    <a href="index.html" class="nav-item nav-link active">Home</a>
+                                    <a href="about.html" class="nav-item nav-link">About</a>
+                                    <a href="service.html" class="nav-item nav-link">Services</a>
+                                    <div class="nav-item dropdown">
+                                        <a href="#" class="nav-link" data-bs-toggle="dropdown">
+                                            <span class="dropdown-toggle">Products</span>
+                                        </a>
+                                        <div class="dropdown-menu">
+                                            <a href="project.html" class="dropdown-item">Bouquets </a>
+                                            <a href="team.html" class="dropdown-item">Indoor Plants</a>
+                                            
+                                        </div>
+                                    </div>
+                                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                                    <div class="nav-btn ps-3">
+                                        <!-- Search Bar Start -->
+                                <form class="d-flex">
+                                    <input class="form-control me-2 search-bar" type="search" placeholder="Search for flowers or plants..." aria-label="Search">
+                                    <button class="btn " type="submit">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </form>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
---
+++ pages/home.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Flora-Flowers shop</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Edu+TAS+Beginner:wght@400..700&family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link rel="stylesheet" href="..\assets\lib\animate\animate.min.css"/>
+        <link href="..\assets\lib\owlcarousel\assets\owl.carousel.min.css" rel="stylesheet">
+        
+
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="..\assets\css\NavHerobootstrap.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="..\assets\css\styleNavHero.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+
+             <!-- electrical template top nav hero section html  -->
+        <!-- Navbar & Hero Start -->
+       
+        <!-- Navbar & Hero End -->
+        <?php include('..\includes\navbar.php'); ?>
+        <!-- Carousel Start -->
+        <div class="header-carousel owl-carousel overflow-hidden">
+            <div class="header-carousel-item hero-section">
+               <img src="..\assets\images\top-view-flowers-with-copy-space.jpg" alt="">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row g-4 align-items-center">
+                            <div class="col-lg-7 animated fadeInLeft">
+                                <div class="text-sm-center text-md-start">
+                                    <h1 class="display-2  mb-4" id=welcome>Welcome To Flora</h1>
+                                      <h4 class=" text-uppercase fw-bold mb-4">Where Nature's Beauty Meets Your Special Moments</h4>
+                                    <div class="d-flex justify-content-center justify-content-md-start flex-shrink-0 mb-4">
+                                        <a class="btn  py-3 px-4 px-md-5 me-2 " href="#">Shop Now</a>
+                                        <a class="btn py-3 px-4 px-md-5 ms-2" href="#">Contact Us </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-carousel-item hero-section">
+                <img src="..\assets\images\flat-lay-blooming-flowers-with-copy-space.jpg" alt="">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row g-4 align-items-center">
+                            <div class="col-lg-7 animated fadeInLeft">
+                                <div class="text-sm-center text-md-start">
+                                   
+                                    <h1 class="display-2  mb-4" style="margin-top: 150px;margin-left:150px" >Daily Fresh Flowers </h1>
+                                    
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+          
+        </div>
+        <!-- Carousel End -->
+
+         <!-- electrical template top nav hero section html end  -->
+
+         <?php include('about.php'); ?>
+
+
+        <!-- electrical template top nav hero section js links  -->
+        <!-- JavaScript Libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="..\assets\lib\wow\wow.min.js"></script>
+        <script src="..\assets\lib\easing\easing.min.js"></script>
+        <script src="..\assets\lib\waypoints\waypoints.min.js"></script>
+        <script src="..\assets\lib\owlcarousel\owl.carousel.min.js"></script>
+        
+
+        <!-- Template Javascript -->
+        <script src="..\assets\js\main.js"></script>
+        <!-- electrical template top nav hero section js links  end-->
+</body>
+</html>
